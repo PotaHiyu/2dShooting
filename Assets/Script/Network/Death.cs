14220ee5307e8dafe88daa5d5aac5f2c31e0f936
Matthew Fitzgibbons <shostakun@schubert.local>
2024-12-21 18:03:43 +0900

    Death animation.

--- Assets/Script/Network/Death.cs
+++ Assets/Script/Network/Death.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Health))]
 public class Death : NetworkBehaviour
 {
+    public GameObject zoomCamera;
+    public float zoomTime = 2f;
     public GameObject deathVFX;
     public float deathTime = 2f;
-    public bool isPlayer = false;
-    
+    float deathTimeElapsed = 0f;
+
     void Start()
     {
         Health health = GetComponent<Health>();
         if (health == null) return;
         health.onPlayerDied += OnPlayerDied;
+        if (zoomCamera == null) zoomCamera = GameObject.FindWithTag("MainCamera");
     }
 
     public void OnPlayerDied()
@@ -22,23 +26,47 @@ public class Death : NetworkBehaviour
         if (isClient) StartCoroutine(DeathAnimation());
     }
 
-    IEnumerator DeathAnimation()
-    {   
-        if (deathVFX) SpawnVFX();
-        if (isPlayer) {
-            Renderer renderer = GetComponent<SpriteRenderer>();
-            if (renderer != null) renderer.enabled = false;
-        }
-        else if (!isPlayer)
-        {
-            gameObject.SetActive(false);
-        }
-        yield return new WaitForSeconds(deathTime);
+    public static float Ease(float t)
+    {
+        if (t < 0.5f) return 8f * t * t * t * t;
+        var x = -2 * t + 2;
+        return 1 - x * x * x * x / 2;
     }
 
-
-    void SpawnVFX()
+    IEnumerator DeathAnimation()
     {
-        Instantiate(deathVFX, transform.position, Quaternion.identity);
+        if (!isClient) yield break;
+
+        float endTime = Time.time + zoomTime;
+        Vector3 goal = transform.position;
+        goal.z = zoomCamera.transform.position.z;
+        var camera = zoomCamera.GetComponent<Camera>();
+        var startSize = camera.orthographicSize;
+        while (zoomCamera != null && Time.time < endTime)
+        {
+            // Change the number to change the movement speed.
+            zoomCamera.transform.position = Vector3.Lerp(zoomCamera.transform.position, goal, 0.005f);
+            // Change the number to change the zoom start. 1 starts immediately; larger numbers delay the start.
+            var zoomProgress = endTime == Time.time ? 1 : Mathf.Clamp01(1 - 1.3f * (endTime - Time.time) / zoomTime);
+            // Change the number to change the zoom amount. Smaller numbers zoom in more.
+            camera.orthographicSize = Mathf.Lerp(startSize, 3, Ease(zoomProgress));
+            yield return null;
+        }
+
+        if (deathVFX) Instantiate(deathVFX, transform.position, Quaternion.identity);
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        if (renderer != null) renderer.enabled = false;
+
+        yield return new WaitForSeconds(deathTime);
+
+        Debug.Log($"Disconnecting! {isClient} {isServer} {isLocalPlayer} {connectionToServer}");
+        connectionToServer?.Disconnect();
+        // SceneManager.LoadScene("Title", LoadSceneMode.Additive);
+    }
+
+    override public void OnStopClient()
+    {
+        Debug.Log("got stop message");
+        SceneManager.LoadScene("Title");
     }
 }
